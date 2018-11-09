5434634bf864cf1d810642eb08a33f5234394286
liuzhengkai <1968410897@qq.com>
2018-11-09 15:33:55 +0800

    upData

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './index';
+import App from 'view/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
